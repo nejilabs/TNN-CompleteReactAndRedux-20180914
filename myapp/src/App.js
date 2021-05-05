608e4fa2ee26300d21b92b2d823f8a272bd340ad
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-05 23:43:48 +0800

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+import React, { Component } from 'react'
+import Ninjas from "./components/Ninjas"
+class App extends Component {
+  // Start: State
+  state = {
+    ninjas: [
+      { id: 1, name: "name1", age: 1, belt: "belt1" },
+      { id: 2, name: "name2", age: 2, belt: "belt2" },
+      { id: 3, name: "name3", age: 3, belt: "belt3" },
+    ]
+  }
+  // End: State
+
+  // Start: Render Template
+  render() {
+    return (
+      <div className="App">
+        App
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
+  // End: Render Template
+
 }
 
 export default App;
