4c5a69dc42f80fcf622baa00900f7f92a6372b36
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-07 01:32:32 +0800

    follow up

--- 5-poketimes/src/pages/Post.js
+++ 5-poketimes/src/pages/Post.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
+
+import { connect } from 'react-redux'
 
 class Post extends Component {
   // Start:State
-  state = {
-    post: null
-  }
+  // state = {
+  //   post: null
+  // }
   // End:State
 
   // Start:Lifecycle Methods
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
+  // componentDidMount() {
+  //   let id = this.props.match.params.post_id;
 
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(res => {
-        this.setState({
-          post: res.data
-        })
-      })
-  }
+  //   axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  //     .then(res => {
+  //       this.setState({
+  //         post: res.data
+  //       })
+  //     })
+  // }
   // End:Lifecycle Methods
 
   // Start:Methods
+  handleClick = (e) => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/'); //redirects user back to home page after Delete
+  }
   // End:Methods
 
   // Start:Template
   render() {
-
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>Delete Post</button>
+        </div>
       </div>
     ) : (<div className="center">Loading post...</div>)
 
@@ -45,4 +53,23 @@ class Post extends Component {
 
 }
 
-export default Post
+const mapStateToProps = (state, ownProps) => {
+  console.log(state)
+  console.log(ownProps)
+
+  let id = ownProps.match.params.post_id;
+  let post = state.posts.find(post => post.id === parseInt(id));
+  return {
+    post: post
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => {
+      dispatch({ type: "DELETE_POST", id: id })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
