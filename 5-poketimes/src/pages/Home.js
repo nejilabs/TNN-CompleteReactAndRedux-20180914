e4cd44a3c5c9b67d3d2b84aff0e3c0c1d70a5e76
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-06 16:33:15 +0800

    25axios@e

--- 5-poketimes/src/pages/Home.js
+++ 5-poketimes/src/pages/Home.js
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+class Home extends Component {
+  // Start:State
+  state = {
+    posts: []
+  }
+  // End:State
 
-const Home = (props) => {
-  setTimeout(() => {
-    props.history.push('/about')
-  }, 2000)
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Home</p>
-    </div>
-  )
+  // Start:Lifecycle Methods
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      })
+  }
+  // End:Lifecycle Methods
+
+  // Start:Methods
+  // End:Methods
+
+  // Start:Template
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No Posts Yet</div>
+    )
+
+
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    )
+  }
+  // End:Template
+
 }
 
 export default Home
