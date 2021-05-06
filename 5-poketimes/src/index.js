f3ae6fb0e183c0dc03d43dc54d00b522e117047f
nellyXinwei <nelsonalbajr@yahoo.com>
2021-05-06 23:33:33 +0800

    Update index.js

--- 5-poketimes/src/index.js
+++ 5-poketimes/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Start: Redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/rootReducers'
+// End: Redux
+
+const store = createStore(rootReducer);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
